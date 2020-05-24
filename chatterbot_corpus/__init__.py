5ac03b14f894946b701d0f2c4a5612c129a65fbe
Hari Prasad <hari@cryocrane.com>
2020-05-24 19:23:46 +0530

    Add files via upload

---
+++ chatterbot_corpus/__init__.py
@@ -0,0 +1,8 @@
+"""
+A machine readable multilingual dialog corpus.
+"""
+
+__version__ = '1.2.0'
+__author__ = 'Tannaver'
+__email__ = 'tannaverha@gmail.com'
+__url__ = 'https://github.com/Tannaver/happybot-corpus-2.0'
